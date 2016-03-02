623a396875428c6b7be209ef93a95b49f8f17e74
Deepflex <saddist42@gmail.com>
2016-03-02 08:02:35 +0700

    Stable #01
    CM12.1 MT6753 P6000 Pro Vendor

---
+++ p6000_pro-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/p6000_pro/vendor,system/vendor)
